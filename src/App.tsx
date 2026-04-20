5b71c9cb0e73878ddfcb2e717df59e879b42f103
Akhilesh Sharma <akhileshsharma.ldh2004@gmail.com>
2026-04-20 18:43:59 +0530

    Styled disabled state of MUI Slider using sx and Mui-disabled class

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import { Button, CssBaseline, TextField, Slider, Stack, Container } from '@mui/material'
-import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import { CssBaseline, Slider, Stack, Container } from '@mui/material'
 
 function App() {
 
 
 
   return (
-    <Container maxWidth="xs">
+    <Container
+    sx = {{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}
+    maxWidth="xs">
       <Stack >
         <CssBaseline />
-        <TextField />
-        <Slider />
-        <Button startIcon={<AutoAwesomeIcon />} variant="contained">
-          Submit
-        </Button>
+        <Slider
+        disabled sx={{width:100,
+           color:'success.main',
+          //  ':hover':{
+          //   backgroundColor:'yellow'
+          //  },
+          //  "& .MuiSlider-thumb" : {
+          //   ":hover":{
+          //     backgroundColor:"blue"
+          //   },
+          //   backgroundColor: "red",
+          //  },
+          "&.Mui-disabled" : {
+            "& .MuiSlider-thumb" : {
+              backgroundColor: "error.main",
+            },
+           },
+        }}/>
       </Stack>
     </Container>
   )
